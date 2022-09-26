a545994453d12fe78cf5d1553ecc79ca6860e1f4
Retourned <tobiasdoorn@gmail.com>
2022-09-26 10:37:30 -0700

    Update index.php

--- index.php
+++ index.php
@@ -4,9 +4,19 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Controller API</title>
 </head>
 <body>
-    
+    <div class="list">
+        <div class="slide1">
+
+        </div>
+        <div class="slide2">
+
+        </div>
+        <div class="slide3">
+            
+        </div>
+    </div>
 </body>
 </html>
