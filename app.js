549d4af6f2aa7821d7831cf2fe4da5a0dca7fa05
Sammie Sayers <sammy@fiks.online>
2022-09-27 22:05:21 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log("yo")
+window.addEventListener("gamepadconnected", event => {
+    console.log("Gamepad connected ");
+    console.log(event.gamepad);
+  });
+
+  window.addEventListener("gamepaddisconnected", event => {
+    console.log("Gamepad disconnected ");
+    console.log(event.gamepad);
+  });   
